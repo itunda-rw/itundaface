e0f8e25c46844ae89a96253feaf60d4a3cf33772
partoftrue <partoftrue@gmail.com>
2026-09-17 17:01:33 +0900

    Expose visual quality rules from canonical entrypoint

--- react/canonical.tsx
+++ react/canonical.tsx
@@ -12,6 +12,7 @@ export * from './finance';
 export * from './culture';
 export * from './state';
 export * from './validation';
+export * from './quality';
 
 // Preserve legacy public exports that do not conflict with canonical family APIs.
 export const {
